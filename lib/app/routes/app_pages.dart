2fe6eacd8440531f810fcbe9bdfc0f069aacd0bb
panam <panamdodia945@gmail.com>
2023-09-27 16:14:36 +0530

    changes

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/login_page/bindings/login_page_binding.dart';
+import '../modules/login_page/views/login_page_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.LOGIN_PAGE;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN_PAGE,
+      page: () => LoginPageView(),
+      binding: LoginPageBinding(),
+    ),
+  ];
+}
